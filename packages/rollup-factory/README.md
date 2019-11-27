1dbf59d98aee92524de4b512ea1328b607e0b932
张怀 <zh5172@qq.com>
2019-11-27 10:52:56 +0800

    chore(rollup-factory): rollup-factory

---
+++ packages/rollup-factory/README.md
@@ -0,0 +1,11 @@
+# `@tools/rollup-factory`
+
+> TODO: description
+
+## Usage
+
+```
+const rollupFactory = require('@tools/rollup-factory');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/rollup-factory/src/index.js
@@ -0,0 +1,32 @@
+const babel = require("rollup-plugin-babel");
+const resolve = require("rollup-plugin-node-resolve");
+const commonjs = require("rollup-plugin-commonjs");
+
+module.exports.rollupFactory = ({ umdName, plugins = [] }) => ({
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/index.bundle.js',
+            format: "umd",
+            name: umdName,
+            sourcemap: true
+        },
+        {
+            file: 'dist/index.esm.js',
+            format: "es",
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        ...plugins,
+        resolve({
+            browser: true,
+            extensions: ['.mjs', '.js', '.jsx', '.json', '.ts', '.tsx'],
+        }),
+        commonjs(),
+        babel({
+            configFile: "../../babel.config.js",
+            extensions: ["js", "jsx", "ts", "tsx"]
+        })
+    ]
+});
