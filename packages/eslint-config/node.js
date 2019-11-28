454848b9d75a99bd111e0cc1f74592438a1a43bf
tinytot1 <zh5172@qq.com>
2019-11-28 15:53:10 +0800

    chore(tools): add eslint config node

---
+++ packages/eslint-config/node.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: false
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-var-requires": "off"
+  }
+};
