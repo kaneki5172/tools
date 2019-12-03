295943a494ec062f26f0426d0d274e8afc22aa9d
tinytot1 <zh5172@qq.com>
2019-12-03 20:24:34 +0800

    feat(@tinytot/http): 增加request interceptors

--- packages/http/src/fetch.ts
+++ packages/http/src/fetch.ts
@@ -1,6 +1,14 @@
+import { interceptors, NextInterceptor } from "./interceptorManager";
+const { request } = interceptors;
+
 const defaultConfig: any = {};
 
 export const fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
   const req = new Request(input, Object.assign(defaultConfig, init));
-  return self.fetch(req);
+  let last = (req: Request): Promise<Response> => self.fetch(req);
+  for (const inter of request.interceptors.slice().reverse()) {
+    const next: NextInterceptor = last;
+    last = req => inter(req, next);
+  }
+  return last(req);
 };
