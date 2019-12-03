b05252ab01b98aa7b883882312a79d479fe50c1c
tinytot1 <zh5172@qq.com>
2019-12-03 09:56:43 +0800

    chore(tools): add babel-preset-config

--- babel.config.js
+++ babel.config.js
@@ -5,19 +5,5 @@ module.exports = {
     // Also consider monorepo packages "root" and load their .babelrc files.
     "./packages/*"
   ],
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        modules: false,
-        useBuiltIns: "usage",
-        corejs: {
-          version: 3
-        }
-      }
-    ],
-    [
-      "@babel/preset-typescript"
-    ]
-  ]
-}
+  presets: ["@tinytot/babel-preset-config"]
+};
