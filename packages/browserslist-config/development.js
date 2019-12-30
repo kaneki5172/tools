07995e80274986135f031196395586b915755242
张怀 <zh5172@qq.com>
2019-12-30 13:17:43 +0800

    feat(browserslist-config): 兼容 > 1%

--- packages/browserslist-config/development.js
+++
@@ -1,3 +0,0 @@
-// Refs https://github.com/browserslist/browserslist#shareable-configs
-// Refs https://github.com/browserslist/browserslist#configuring-for-different-environments
-module.exports = ["last 2 Chrome versions"];
