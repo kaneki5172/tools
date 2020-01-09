316e7dbcfe92ddb8bcaf3a04116092186474fe1c
张怀 <zh5172@qq.com>
2020-01-09 13:17:50 +0800

    fix(webpack-config-factory): 修复css module

--- packages/webpack-config-factory/src/bricks/webpack.common.js
+++ packages/webpack-config-factory/src/bricks/webpack.common.js
@@ -56,8 +56,9 @@ module.exports = () => {
           use: [
             "style-loader",
             ...getStyleLoaders({
-              modules: true,
-              localIdentName: "[local]--[hash:base64:8]"
+              modules: {
+                localIdentName: "[local]--[hash:base64:8]"
+              }
             })
           ]
         },
