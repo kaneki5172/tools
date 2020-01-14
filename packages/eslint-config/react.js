4d96085c22f27ce151b56a3489ca5f32350801d7
张怀 <zh5172@qq.com>
2020-01-14 15:32:25 +0800

    feat(eslint-config): add react-hooks rules

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -5,5 +5,9 @@ module.exports = {
     es6: true,
     browser: true,
     node: false
+  },
+  rules: {
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
   }
 };
